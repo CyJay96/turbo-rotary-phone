f86e4325ad9d61d3b92a93fd380ed5ea7b86ec8e
CyJay96 <konstantin201586@gmail.com>
2022-07-24 23:14:02 +0300

    Initial commit

---
+++ src/main/java/com/example/turborotaryphone/TurboRotaryPhoneApplication.java
@@ -0,0 +1,13 @@
+package com.example.turborotaryphone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TurboRotaryPhoneApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TurboRotaryPhoneApplication.class, args);
+    }
+
+}
