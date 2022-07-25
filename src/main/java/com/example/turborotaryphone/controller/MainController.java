90cf986dd2d19c6189e6e05d504c9aa24fd78335
CyJay96 <konstantin201586@gmail.com>
2022-07-25 15:40:27 +0300

    Added DB for messages with filter

--- src/main/java/com/example/turborotaryphone/controller/MainController.java
+++ src/main/java/com/example/turborotaryphone/controller/MainController.java
@@ -1,12 +1,21 @@
 package com.example.turborotaryphone.controller;
 
+import com.example.turborotaryphone.model.Message;
+import com.example.turborotaryphone.repos.MessageRepo;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class MainController {
+
+    private final MessageRepo messageRepo;
+
+    public MainController(MessageRepo messageRepo) {
+        this.messageRepo = messageRepo;
+    }
 
     @GetMapping("/greeting")
     public String greeting(@RequestParam(name="name", required = false, defaultValue = "World") String name, Model model) {
@@ -16,7 +25,36 @@ public class MainController {
 
     @GetMapping
     public String main(Model model) {
-        model.addAttribute("some", "This is TurboRotaryPhone!");
+        Iterable<Message> messages = messageRepo.findAll();
+        model.addAttribute("messages", messages);
+        return "main";
+    }
+
+    @PostMapping
+    public String add(@RequestParam String text,
+                      @RequestParam String tag,
+                      Model model) {
+        Message message = new Message(text, tag);
+        messageRepo.save(message);
+
+        Iterable<Message> messages = messageRepo.findAll();
+        model.addAttribute("messages", messages);
+
+        return "main";
+    }
+
+    @PostMapping("/filter")
+    public String filter(@RequestParam String filter, Model model) {
+        Iterable<Message> messages;
+
+        if (filter != null && !filter.isEmpty()) {
+            messages = messageRepo.findByTag(filter);
+        } else {
+            messages = messageRepo.findAll();
+        }
+
+        model.addAttribute("messages", messages);
+
         return "main";
     }
 
