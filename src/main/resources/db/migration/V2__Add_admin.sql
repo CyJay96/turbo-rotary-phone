86bb17bd7383f88e53e717c12e32b1e4c6c82e5b
CyJay96 <konstantin201586@gmail.com>
2022-08-04 18:43:08 +0300

    Fixed FlyWay

---
+++ src/main/resources/db/migration/V2__Add_admin.sql
@@ -0,0 +1,6 @@
+insert into usr (id, username, password, email, active) values
+    (1, 'admin', '$2a$08$AuLMiIojO3jh1acNoj1djuGzr1Q0bgH0e0YwMOvmk9gnTPRyJEwAa', 'admin@admin.admin', true);
+-- password = admin
+
+insert into user_role (user_id, roles) values
+    (1, 'USER'), (1,  'ADMIN');
